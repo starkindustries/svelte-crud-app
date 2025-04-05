9e92bf836ed930a21dd7615b20e9b80b5afff376
Zion <zionperez@gmail.com>
2025-04-04 19:00:21 -0700

    add coverage command to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	// Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
-	resolve: process.env.VITEST
-		? {
-				conditions: ['browser']
-			}
-		: undefined
+	resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
+	test: {
+		coverage: {
+			exclude: [
+				'svelte.config.js',
+			]
+		}
+	}
 });
